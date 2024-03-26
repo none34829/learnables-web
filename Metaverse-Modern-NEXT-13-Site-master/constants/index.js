92cc98cdcc39ac7e645202b2431939063cad7c93
none34829 <133172306+none34829@users.noreply.github.com>
2024-03-26 17:17:57 +0530

    neww

--- Metaverse-Modern-NEXT-13-Site-master/constants/index.js
+++ Metaverse-Modern-NEXT-13-Site-master/constants/index.js
@@ -42,9 +42,9 @@ export const exploreWorlds = [
 ];
 
 export const startingFeatures = [
-  "Find a world that suits you and you want to enter",
-  "Enter the world by reading Har Har Mahadev to be safe",
-  "No need to beat around the bush, just stay on the gas and have fun",
+  "Step 1",
+  "Step 2",
+  "Step 3",
 ];
 
 export const newFeatures = [
