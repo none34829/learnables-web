d525dcdd398636714e822c79dfa18ae7aaa6528f
none34829 <133172306+none34829@users.noreply.github.com>
2024-04-09 00:15:44 +0530

    st

--- Metaverse-Modern-NEXT-13-Site-master/sections/Hero.jsx
+++ Metaverse-Modern-NEXT-13-Site-master/sections/Hero.jsx
@@ -44,10 +44,6 @@ const Hero = () => (
           className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative "
         />
 
-        <a href="#explore">
-          <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10 ">
-          </div>
-        </a>
       </motion.div>
     </motion.div>
   </section>
